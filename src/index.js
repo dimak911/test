770d5a80ea515d8900f27d9804d30446e3adfa43
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-07-17 18:08:29 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const result = axios.get(
-  'https://api.mediastack.com/v1/news?access_key=755d8a8e2e53f396865281416825613b'
+  'http://api.mediastack.com/v1/news?access_key=755d8a8e2e53f396865281416825613b'
 );
 result.then(res => {
   console.log(res.data);
