65876c893e7391439b7f04df6cd224d771f311b3
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-07-17 19:09:38 +0300

    test

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const result = axios.get(
-  'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=7V2Mdku3K6jAwbEoNcKctzHC7q7RRQcQ'
-);
-result.then(res => {
-  console.log(res.data);
-});
